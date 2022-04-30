bdbcb4191edc9bf44a0672623373ee909483b43c
nmayol <02mayolneus@gmail.com>
2022-04-30 15:46:28 +0200

    afegir les nostres coses

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,5 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-        
-      </Head>
-
-      <main>
-        <Header title="Welaaaacome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+  return ()
 }
