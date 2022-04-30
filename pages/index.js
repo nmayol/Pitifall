357f25e6c6bffa0c9f11405d70db29d23cdb6526
nmayol <02mayolneus@gmail.com>
2022-04-30 15:42:58 +0200

    afegir les nostres coses

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,12 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
-        <script src="https://cdn.jsdelivr.net/npm/phaser@3.15.1/dist/phaser-arcade-physics.min.js"></script>
+        
       </Head>
 
       <main>
-        <Header title="Welaaaaaaaaaaaaaaaaaaaaaacome to my app!" />
+        <Header title="Welaaaacome to my app!" />
         <p className="description">
-        <p 
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
