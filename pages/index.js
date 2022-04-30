20e1e1a872b1f92aa4eddbe56156a196739d0764
nmayol <02mayolneus@gmail.com>
2022-04-30 16:20:12 +0200

    afegir les nostres coses definitiu?

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,6 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <head>
-    <script src="https://cdn.jsdelivr.net/npm/phaser@3.15.1/dist/phaser-arcade-physics.min.js"></script>
-</head>
-<body>
-
-    <script>
     var config = {
         type: Phaser.AUTO,
         width: 480,
@@ -38,8 +32,5 @@ export default function Home() {
     function update () {
 
     }
-    </script>
-
-</body>
   )
 }
